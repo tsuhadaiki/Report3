62afbd901e627550ecb8113d2917a76b03f87f00
Daiki Tsuha <e175730@ie.u-ryukyu.ac.jp>
2017-11-13 13:35:32 +0900

    LivingThingクラスの作成及び他クラスの重複部分改定

--- src/main/jp/ac/uryukyu/ie/e175730/Hero.java
+++ src/main/jp/ac/uryukyu/ie/e175730/Hero.java
@@ -8,24 +8,10 @@ package jp.ac.uryukyu.ie.e175730;
  *  boolean dead; //敵の生死状態。true=死亡。
  * Created by tnal on 2016/11/13.
  */
-public class Hero {
-    String name;
-    int hitPoint;
-    int attack;
-    boolean dead;
+public class Hero extends LivingThing {
 
-    /**
-     * コンストラクタ。名前、最大HP、攻撃力を指定する。
-     * @param name ヒーロー名
-     * @param maximumHP ヒーローのHP
-     * @param attack ヒーローの攻撃力
-     */
     public Hero (String name, int maximumHP, int attack) {
-        this.name = name;
-        hitPoint = maximumHP;
-        this.attack = attack;
-        dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+        super (name,maximumHP , attack);
     }
 
     /**
@@ -45,11 +31,6 @@ public class Hero {
      * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
      * @param e 攻撃対象
      */
-    public void attack(Enemy e){
-        int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.getName(), damage);
-        e.wounded(damage);
-    }
 
     /**
      * 自身へ攻撃されたときのダメージ処理をするメソッド。

a87fea580fd3f2b35eb59b933f7ab5e7b7ea012c
Daiki Tsuha <e175730@ie.u-ryukyu.ac.jp>
2017-11-13 20:41:37 +0900

    testのセッターゲッターでやるやつ

--- src/test/jp/ac/uryukyu/ie/e175730/EnemyTest.java
+++ src/test/jp/ac/uryukyu/ie/e175730/EnemyTest.java
@@ -1,8 +1,8 @@
 package jp.ac.uryukyu.ie.e175730;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 
 public class EnemyTest {
     /**
@@ -13,11 +13,10 @@ public class EnemyTest {
         int heroHP = 10;
         Hero hero = new Hero("テスト勇者", heroHP, 5);
         Enemy enemy = new Enemy("テストスライム", 6, 3);
-        enemy.dead = true;
+        enemy.setDead(true);
         for(int i=0; i<10; i++) {
             enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
         }
-        assertEquals(heroHP, hero.hitPoint);
+        assertEquals(heroHP, hero.getHitPoint());
     }
-
 }
